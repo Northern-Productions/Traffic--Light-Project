4616017edeef3711f1a1447f07145245420b6892
Rob Schultz <Robschultz4444@yahoo.com>
2024-02-19 11:30:48 -0600

    finished traffic light exercise

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,36 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+    state = {
+      currentColor: 2,
+    };
+
   render() {
+    const { currentColor } = this.state;
+
+    const greenLight = currentColor === 0 ? "green" : "black";
+    const yellowLight = currentColor === 1 ? "yellow" : "black";
+    const redLight = currentColor === 2 ? "red" : "black";
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${redLight}`}></div>
+          <div className={`circle ${yellowLight}`}></div>
+          <div className={`circle ${greenLight}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button
+          className="next-state-button"
+          onClick={() => {
+            this.setState(() => ({
+              currentColor: (currentColor + 1) % 3,
+            }));
+          }}
+        >
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+
+  const [currentColor, setCurrentColor] = useState(2);
+
+  const greenLight = (currentColor === 0) ? "green" : "black";
+  const yellowLight = (currentColor === 1) ? "yellow" : "black";
+  const redLight = (currentColor === 2) ? "red" : "black";
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${redLight}`}></div>
+        <div className={`circle ${yellowLight}`}></div>
+        <div className={`circle ${greenLight}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={() => {
+        setCurrentColor((currentColor + 1) % 3);
+      }}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
